f371e2502d69b045f7453b3bd8e730d7379f433f
Carl Hain <crhain@gmail.com>
2019-06-23 12:06:31 -0500

    Hook up LanguageContext to Button component and console.log default vaule when component renders

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import LanguageContext from '../context/LanguageContext';
 
 class Button extends React.Component {
+  //hook up context object to class component
+  static contextType = LanguageContext;
+
   render() {
+    console.log(this.context);
     return <button className="ui button primary">Submit</button>;
   }
 }
